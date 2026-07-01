45ef1141618022506841491fd00c6bd228411dca
Keita Saito <saitou-keita0109@outlook.jp>
2026-07-01 15:01:10 +0900

    プレイヤーにモーションを設定

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator animator_;
+
+    void Start()
+    {
+        animator_ = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+
+    }
+}
